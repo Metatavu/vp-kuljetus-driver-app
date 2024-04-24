b34acd3b6e16fa376828a25545fac401959874ad
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-24 10:09:28 +0300

    Testing develop

--- lib/app/openapi_config.dart
+++ lib/app/openapi_config.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-04-23T09:41:21.408571
+// Openapi Generator last run: : 2024-04-24T09:27:04.483875
 import "package:openapi_generator_annotations/openapi_generator_annotations.dart";
 
 @Openapi(

d71793b4b0c11f1ad7a59b59cc0edd6d78f94d28
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2024-06-12 11:58:54 +0300

    Added submodule and updated generator path

--- lib/app/openapi_config.dart
+++ lib/app/openapi_config.dart
@@ -1,9 +1,9 @@
-// Openapi Generator last run: : 2024-06-12T11:25:35.964773
+// Openapi Generator last run: : 2024-06-12T11:58:30.808283
 import "package:openapi_generator_annotations/openapi_generator_annotations.dart";
 
 @Openapi(
   inputSpec: InputSpec(
-    path: "../specs/specs/app.yaml",
+    path: "./specs/specs/app.yaml",
   ),
   generatorName: Generator.dio,
   outputDirectory: "./packages/tms_api",

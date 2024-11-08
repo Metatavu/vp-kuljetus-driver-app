e610ac1d5a96067ef0aadda32474944d48c0b899
Ville Juutila <ville.juutila@gmail.com>
2024-11-08 22:05:16 +0200

    Generated new specs

--- lib/app/openapi_config.dart
+++ lib/app/openapi_config.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-11-06T09:41:54.683982
+// Openapi Generator last run: : 2024-11-08T21:24:19.048596
 import "package:openapi_generator_annotations/openapi_generator_annotations.dart";
 
 @Openapi(
